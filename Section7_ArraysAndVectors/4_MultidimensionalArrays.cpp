c0be113409c33571006efcf86e155c5da1d99886
HuyVoDinh <huy.vd76@gmail.com>
2023-12-11 21:51:50 +0700

    Add multidimensional array

---
+++ Section7_ArraysAndVectors/4_MultidimensionalArrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /***
+     * [Arrays]
+     * Declaring multi-dimensional arrays
+     * There's no real limit to the number of dimensions you can have for arrays, be aware that some compliers do place limits on this.
+     * Though the limits are quite high and you're unlikely to reach them
+     * 
+     * The syntax for declaring multi-dimensional arrays just involves adding another set of square brackets with the new dimension inside them
+     * 
+     * Element_Type array_name [dim1_size][dìm_size]
+     * 
+     *      int movie_rating [3][4];
+     * 
+     *      const int rows {3};
+     *      const int cols {4};
+     *      int movie_rating [rows][cols]
+     *      {
+     *          {0, 4, 3, 5}.
+     *          {2, 3, 3, 5}.
+     *          {1, 4, 4, 5}
+     *      };
+     * ***/
+}
