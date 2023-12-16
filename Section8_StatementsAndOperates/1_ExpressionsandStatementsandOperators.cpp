aeab4ef9650a0c6349f05018651ac49cc3c171ff
HuyVoDinh <huy.vd76@gmail.com>
2023-12-16 21:55:09 +0700

    Add Expressions, Statements and Operators of section 8

---
+++ Section8_StatementsAndOperates/1_ExpressionsandStatementsandOperators.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+/***
+ * [Expressions]
+ * An expression is:
+ *  The most basic building block of a program
+ *  "a sequence of operators and operands that specifies a computation"
+ *  Computes a value from a number of operands
+ *  There is much, much more to expressions - not necessary at this level
+ * 
+ *  Expressions - examples
+ *      34                      // literal
+ *      favorite_number         // variable
+ *      1.5 + 2.5               // addition
+ *      2 * 5                   // multiplication
+ *      a > b                   // relational
+ *      a = b                   // assignment
+ * 
+ * [Statements]
+ * A statement is:
+ *      A complete line of code that performs some action
+ *      Usually terminated with a semi-colon
+ *      Usually contain expressions
+ *      C++ has many types of statements
+ *          expression, null, compound, selection, iteration, declaration, jump, try blocks, and others.
+ * 
+ * Example:
+ *      int x;                                          // declaration
+ *      favorite_number = 12;                           // assignment
+ *      1.5 + 2.8;                                      // expression
+ *      x = 2 * 5;                                      // assignment
+ *      if (a > b) cout << "a is greater than b";       //if
+ * 
+ * [Using Operators]
+ * C++ has a rich set of operators
+ *      unary, binary, ternary
+ * Common operators can be grouped as follows:
+ *      assignment => modify the value of some object by assigning a new value to it
+ *      arithmetic => perform mathematical operations on operands
+ *      increment/decrement => part of them work like an assignment and part of them work like arithmetic
+ *      relational => compare the values of two objects
+ *      logical => test for logical or boolean conditions
+ *      member access =>
+ *      other
+ * **/
+
+
+int main()
+{}
