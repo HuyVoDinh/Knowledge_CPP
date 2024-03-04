bc52588f8fc3b140075ff66c5ddbc2322af7ff1b
HuyVoDinh <huy.vd76@gmail.com>
2024-03-04 23:15:55 +0700

    Add - section 8 - Compound Assignment

---
+++ Section8_StatementsAndOperates/12_CompoundAssignment.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /***
+     * |    Operator    |       Example     |       Meaning         |
+     * |============================================================|
+     * |        +=      |   lhs += rhs;     |   lhs = lhs + (rhs);  |
+     * |        -=      |   lhs -= rhs;     |   lhs = lhs - (rhs);  |
+     * |        *=      |   lhs *= rhs;     |   lhs = lhs * (rhs);  |
+     * |        /=      |   lhs /= rhs;     |   lhs = lhs / (rhs);  |
+     * |        %=      |   lhs %= rhs;     |   lhs = lhs % (rhs);  |
+     * |        >>=     |   lhs >>= rhs;    |   lhs = lhs >> (rhs); |
+     * |        <<=     |   lhs <<= rhs;    |   lhs = lhs << (rhs); |
+     * |        &=      |   lhs &= rhs;     |   lhs = lhs & (rhs);  |
+     * |        ^=      |   lhs ^= rhs;     |   lhs = lhs ^ (rhs);  |
+     * |        |=      |   lhs |= rhs;     |   lhs = lhs ! (rhs);  |
+     * |============================================================| 
+     * 
+     * [Example]
+     * 
+     *  lhs op = rhs;           // lhs = lhs op (rhs);
+     *  a += 1;                 // a = a + 1;
+     *  a /= 5;                 // a = a / 5;
+     *  a *= b + c;             // a = a * (b + c);
+     *  cost += items * tax;    // cost = cost + (items * tax);
+     * ***/
+
+    return 0;
+}
