70a84c212667e578a2925b47ba4772a69d200acf
HuyVoDinh <huy.vd76@gmail.com>
2024-03-09 13:45:02 +0700

    Add - Section 9 - Overview Controlling Program Flow

---
+++ Section9_ControllingProgramFlow/1_OverView.cpp
@@ -0,0 +1,26 @@
+/***
+ * [Controlling Program Flow]
+ * 
+ * Sequence
+ *  Ordering statements sequentially
+ * Selection
+ *  Making decisions
+ * Iteration
+ *  Looping or repeating
+ * 
+ * [Selection - Decision making]
+ *  if statement
+ *  if-else statement
+ *  Nested if statements
+ *  switch statement
+ *  Conditional operator ? :
+ * 
+ * [Iteration - Looping]
+ *  for loop
+ *  Range-based for loop
+ *  while loop
+ *  do-while loop
+ *  continue and break
+ *  Infinite loops
+ *  Nested loops
+ * **/
