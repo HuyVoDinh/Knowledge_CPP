11c5c04f92b1c11ca7d5e9ff9538632602a72814
HuyVoDinh <huy.vd76@gmail.com>
2024-03-21 20:29:28 +0700

    Add - section 9 - Overview about looping

---
+++ Section9_ControllingProgramFlow/11_Looping.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/***
+ * [Looping]
+ * 
+ * The third basic building block of programming
+ *      sequence, selection, iteration
+ * Iteration or repetition
+ * Allows the execution of a statement or block of statements repeatedly
+ * Loops are made up a loop condition and the body which contains the statements to repeat
+ * 
+ * Some typical use-cases
+ * 
+ * Execute a loop:
+ *  a specific number of times
+ *  for each element in a collection
+ *  while a specific condition remains true
+ *  until a specific  condition becomes false
+ *  until we reach the end of some input stream
+ *  forever
+ *  many, many more
+ * 
+ * for loop
+ *  iterate a specific number of times
+ * 
+ * Range-based for loop
+ *  one iteration for each element in a range or collection
+ * 
+ * while loop
+ *  iterate while a condition remains true
+ *  stop when the condition becomes false
+ *  check the condition at the beginning of every iteration
+ * 
+ * do-while
+ *  iterate while a condition remains true
+ *  stop when the condition becomes false
+ *  check the condition at the end of every iteration
+ * ***/
+
+
+
+int main()
+{
+    
+    return 0;
+}
