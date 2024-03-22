b1341add2bdeb2415501f35f53f91e559e4f54b8
HuyVoDinh <huy.vd76@gmail.com>
2024-03-22 16:37:35 +0700

    Add - Section 9 - Exercise

---
+++ Section9_ControllingProgramFlow/13_Exercise.cpp
@@ -0,0 +1,20 @@
+/***
+ * Write code that uses a for loop to calculate the sum of the odd integers from 1 to 15, inclusive. The final result should be stored in an integer variable named sum .
+ * 
+ * **/
+
+#include <iostream>
+using namespace std;
+
+int calculate_sum() {
+    int sum = 0;
+    for(int i = 1; i<=15; i++)
+        if(i%2!=0)
+            sum+=i;
+    return sum;
+}
+
+int main()
+{
+    return 0;
+}
