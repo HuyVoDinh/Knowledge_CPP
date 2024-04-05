d8bb2815cee6fbecfb7bd30cfb6c5e93c801f99a
HuyVoDinh <huy.vd76@gmail.com>
2024-04-05 23:27:13 +0700

    Add - Section 10 - Character functions

---
+++ Section10_CharactersAndStrings/2_CharacterFunctions.cpp
@@ -0,0 +1,26 @@
+/***
+ * [Character Functions]
+ * #include <cctype>\
+ * 
+ * 
+ * function_name (char)
+ * 
+ * Functions for testing characters => They evaluate to true or false
+ * Functions for converting character case => from upper to lower or lower to uppercase
+ * 
+ * [Testing characters]
+ * 
+ * isalpha (c)      |   True if c is a letter
+ * isalnum (c)      |   True if c is a letter or digit
+ * isdigit (c)      |   True if c is a digit
+ * islower (c)      |   True if c is a lowercase letter
+ * isprint (c)      |   True if c is a printable character
+ * ispunct (c)      |   True if c is a punctuation character
+ * isupper (c)      |   True if c is a uppercase letter
+ * isspace (c)      |   True if c is a whitespace
+ * 
+ * [Converting characters]
+ * 
+ * tolower (c)      |   returns lowercase of c
+ * toupper (c)      |   returns uppercase of c
+ * ***/
