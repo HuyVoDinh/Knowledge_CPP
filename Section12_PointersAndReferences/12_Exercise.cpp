6213f290310ed683c7ffcb58c393020885055670
HuyVoDinh <huy.vd76@gmail.com>
2024-04-30 21:02:54 +0700

    Add - Section 12 - Exercise

---
+++ Section12_PointersAndReferences/12_Exercise.cpp
@@ -0,0 +1,55 @@
+/***
+ * [Find the Maximum Element in an Array using Pointers]
+ * Exercise: Find Maximum Element
+
+Specification:
+
+Write a C++ function find_max_element that takes an integer array and its size as input and returns the maximum element in the array using pointers.
+
+Function Signature:
+
+int findMaxElement(int* arr, int size);
+Input:
+
+arr: A pointer to the first element of the integer array.
+
+size: The size of the array.
+
+Output:
+
+The function should return the maximum element in the array.
+
+Example:
+
+int arr[] = {12, 45, 67, 23, 9};
+int size = sizeof(arr) / sizeof(arr[0]);
+ 
+int maxElement = findMaxElement(arr, size);
+ 
+// maxElement should be 67
+Constraints:
+
+The array will always have at least one element.
+
+The elements in the array are unique.
+ * ***/
+
+#include <iostream>
+using namespace std;
+
+int find_max_element(int* arr, int size) {
+    //-- Write your code below this line 
+    int max = arr[0];
+    for(int i = 0; i < size; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+    }
+    return max;
+    //-- Write your code below this line 
+}
+
+int main() {
+
+    return 0;
+}
