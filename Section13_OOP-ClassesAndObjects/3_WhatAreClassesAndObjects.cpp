e4a2431d55bc7022991002ce5a52bbeaea9fe632
HuyVoDinh <huy.vd76@gmail.com>
2024-05-01 21:46:32 +0700

    Add - Section 13 - What are classes and objects

---
+++ Section13_OOP-ClassesAndObjects/3_WhatAreClassesAndObjects.cpp
@@ -0,0 +1,40 @@
+/***
+ * [Classes and Objects]
+ * Classes
+ *  blueprint from which objects are created
+ *  a user-defined data-type
+ *  has attributes (data)
+ *  has methods (functions)
+ *  can hide data and methods
+ *  provides a public interface
+ * 
+ * Example classes
+ *  Account
+ *  Employee
+ *  Image
+ *  std::vector
+ *  std::string
+ * 
+ * Objects
+ *  created from a class
+ *  represents a specific instance of a class
+ *  can create many, many objects
+ *  each has its own identity
+ *  each can use the defined class methods
+ * 
+ * Example Account objects 
+ *  Frank's account is an instance of an Account
+ *  Jim's account is an instance of an Account
+ *  Each has its own balance, can make deposits, withdrawals, etc.
+ * 
+ * [Example]
+ * 
+ * int high_score;
+ * int low_score;
+ * 
+ * Account frank_account;
+ * Account jim_account;
+ * 
+ * std::vector<int> scores;
+ * std::string name;
+ * ***/
