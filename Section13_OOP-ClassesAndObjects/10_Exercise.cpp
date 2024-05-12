22bfc09f19aa868638d194849b57a21fe82c7b3f
HuyVoDinh <huy.vd76@gmail.com>
2024-05-12 14:27:02 +0700

    Add - Section 13 - Exercise

---
+++ Section13_OOP-ClassesAndObjects/10_Exercise.cpp
@@ -0,0 +1,11 @@
+/***
+ * [Add more public methods to an existing class]
+ * 
+ * Given an existing Dog class in Dog.h, add the following public method to the class:
+
+get_human_years() return the dog's age * 7
+
+speak()  returns the std::string "Woof"
+
+Please add your code in the Dog.h file.
+ * **/
