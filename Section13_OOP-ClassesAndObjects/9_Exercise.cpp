765b1297fda09b5b48490fdc6c08e92dbf9b2705
HuyVoDinh <huy.vd76@gmail.com>
2024-05-12 14:26:40 +0700

    Add - Section 13 - Exercise

---
+++ Section13_OOP-ClassesAndObjects/9_Exercise.cpp
@@ -0,0 +1,14 @@
+/***
+ * [Adding public methods that access private class attributes]
+ * 
+ * Given the following Dog class in Dog.h add the following public getter and setter methods that get and set each class attribute as follows:
+
+get_name returns the name attribute of the dog 
+
+set_name sets the name attribute of the dog to the string that is passed into the method.
+
+get_age returns the age attribute of the dog
+
+set_age sets the age attribute of the dog to the int passed into the method.
+ * ***/
+
