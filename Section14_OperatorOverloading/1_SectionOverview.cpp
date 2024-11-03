31d583b1098029e22a0d5949ca0f35da9ff7a918
HuyVoDinh <huy.vd76@gmail.com>
2024-11-03 14:35:50 +0700

    [Section 14][Add] Section Overview

---
+++ Section14_OperatorOverloading/1_SectionOverview.cpp
@@ -0,0 +1,17 @@
+/***
+ * [Section Overview]
+ * Operator Overloading
+ * 
+ * What is Operator Overloading?
+ * 
+ * Overloading the assignment operator (=)      // It defines the semantics of what happens when assigned one object to another. C++ provides a default way of doing object assignment much as it did with the copy constructor.
+ * //Example when use raw pointers in our classes, we must provide our own version of the assignment operator. 
+ *  Copy semantics
+ *  Move semantics
+ * 
+ * Overloading operators as member functions
+ * 
+ * Overloading operators as global functions
+ * 
+ * Overloading stream insertion (<<) and extraction operators (>>)
+ * ****/
