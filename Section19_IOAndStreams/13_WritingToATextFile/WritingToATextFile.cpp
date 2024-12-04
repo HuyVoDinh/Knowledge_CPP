7216a5166374c1e358a68f1f2a1689078457dcd5
HuyVoDinh <huy.vd76@gmail.com>
2024-12-05 00:36:44 +0700

    [Section 19][Update] Add code for writing to a text file lesson

--- Section19_IOAndStreams/13_WritingToATextFile/WritingToATextFile.cpp
+++ Section19_IOAndStreams/13_WritingToATextFile/WritingToATextFile.cpp
@@ -133,3 +133,50 @@
  *  in_file.close();            // close the files
  *  out_file.close();
  * ***/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(){
+    std::ofstream out_file1 {"output.txt", std::ios::app}; 
+    if(!out_file1){
+        std::cerr << "Error creating file\n";
+        return 1;
+    }
+
+    std::string line;
+    std::cout << "Enter something to write to the file: ";
+    std::getline(std::cin, line);
+    out_file1 << line << std::endl;
+
+    out_file1.close();
+
+    // Copy file
+    std::ifstream in_file {"../12_Challenge3/challenge.txt"};
+    std::ofstream out_file {"copy.txt"};
+
+    if(!in_file){
+        std::cerr << "Error opening input file\n";
+        return 1;
+    }
+    if(!out_file){
+        std::cerr << "Error opening output file\n";
+        return 1;
+    }
+
+    while (std::getline(in_file, line))
+        out_file << line << std::endl;
+
+    std::cout << "File copied\n";
+
+    // Copy char
+    char c;
+    while(in_file.get(c))
+        out_file.put(c);
+
+    std::cout << "File copied\n";
+    in_file.close();
+    out_file.close();
+    return 0;
+
+}
